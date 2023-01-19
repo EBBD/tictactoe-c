f96b895bb4d3176211830b1fb84577922db6d5a9
EBBD <109923078+EBBD@users.noreply.github.com>
2023-01-19 12:27:59 -0500

    Update main.c

--- main.c
+++ main.c
@@ -106,14 +106,106 @@ int main(int argc, char** argv){
 					countup1 = 0;
 				}
 			}
-			turn = turn + 1;
+			i = 0;
+			turn = 1;
 		}
 		else if(turn == 1){
 			printf("The turn is now O. Player 2, please enter a number from 1-9 that has not already been picked.\n");
 			scanf("%d", &holder);
 		        if (holder == 1){
-
-			}		
+				if (array[0] == 0){
+					array[0] = 2;
+				}
+				else {
+				printf("That number has already been chosen. As such, you forfeit your turn.");
+				}
+			}
+			else if (holder == 2){
+				if (array[1] == 0){
+					array[1] = 2;
+				}
+				else {
+				 printf("That number has already been chosen. As such, you forfeit your turn.");
+										                                }
+			}
+			else if (holder == 3){
+				if (array[2] == 0){
+                                array[2] = 2;
+				}
+				else {
+				 printf("That number has already been chosen. As such, you forfeit your turn.");
+										                                }
+                        }
+			else if (holder == 4){
+				if (array[3] == 0){
+                                array[3] = 2;
+				}
+				else {
+				printf("That number has already been chosen. As such, you forfeit your turn.");
+										                                }
+                        }
+			else if (holder == 5){
+				if (array [4] == 0){
+                                array[4] = 2;
+				}
+				else {
+				printf("That number has already been chosen. As such, you forfeit your turn.");
+										                                }
+                        }
+			else if (holder == 6){
+				if (array[5] == 0){
+                                array[5] = 2;
+				}
+				else {
+				printf("That number has already been chosen. As such, you forfeit your turn.");
+										                                }
+                        }
+			else if (holder == 7){
+				if (array[6] == 0){
+                                array[6] = 2;
+				}
+				else {
+				printf("That number has already been chosen. As such, you forfeit your turn.");
+										                                }
+                        }
+			else if (holder == 8){
+				if (array[7] == 0){
+                                array[7] = 2;
+				}
+				else {
+				printf("That number has already been chosen. As such, you forfeit your turn.");
+										                                }
+                        }
+			else if (holder == 9){
+				if (array [8] == 0){
+                                array[8] = 2;
+				}
+				else {
+				printf("That number has already been chosen. As such, you forfeit your turn.");
+										                                }
+                        }
+			else {
+				printf("Faulty input! Please restart program.");
+			}	
+			while (i < 9){
+				if (array[i] == 0){
+					printf("- ");
+				}
+				else if (array [i] == 1){
+					printf("x ");
+				}
+				else if (array [i] == 2){
+					printf("o ");
+				}	
+				countup2 = countup2 + 1;
+				i = i + 1;
+				if (countup1 >= 3){
+					printf("\n");
+					countup1 = 0;
+				}
+			}
+			i = 0;
+			turn = 0;
 		}
 	}
 }
